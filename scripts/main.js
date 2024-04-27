06a6745f2aea47d40e8b03ec36e54f8016a2f729
Trevor Mustoe <trevormustoe0405@gmail.com>
2024-04-27 16:01:40 -0400

    connected a filter to my buttons and render the cards on dom dynamically

--- scripts/main.js
+++ scripts/main.js
@@ -1,29 +1,26 @@
-import { renderToDom } from "../utils/renderToDom";
-
-
 const studentList = [
   {
     id: 1,
-    student: "Harry",
+    studentName: "Harry",
     house: "Griffindor",
-    img: "https://ik.imagekit.io/hpapi/harry.jpg"
+    img: "https://cdn.pixabay.com/photo/2019/03/22/18/14/harry-potter-4073867_1280.png"
   },
   {
     id: 2,
-    student: "Ron",
+    studentName: "Ron",
     house: "Ravenclaw",
     img: "https://ik.imagekit.io/hpapi/ron.jpg",
   },
   {
     id: 3,
-    student: "Jeff",
+    studentName: "Jeff",
     house: "Hufflepuff",
     img: "https://ik.imagekit.io/hpapi/cedric.png"
   },
   {
     id: 4,
-    student:  "John Cena",
-    house: "Slytherine",
+    studentName:  "John Cena",
+    house: "Snake",
     img: "https://ik.imagekit.io/hpapi/goyle.jpg"
   }
   ]
@@ -33,8 +30,8 @@ const studentCard = (object) => {
   return  ` 
   <div class="card" id="card-container" >
       <div class="card-body">
-      <h5 id="cardTitle" class="card-title">${object.name}</h5>
-      <img class="card-img-top" src="${object.imageUrl}" alt="pet picture" style="width: 100%;">
+      <h5 id="cardTitle" class="card-title">${object.studentName}</h5>
+      <img class="card-img-top" src="${object.img}" alt="Student IMG" style="width: 200px;">
         <p id="cardText" class="card-text">${object.house}</p>
         <hr></hr>
         <button class="btn btn-danger" id="expel--${object.id}">EXPEL</button>
@@ -53,34 +50,74 @@ const studentCard = (object) => {
     };
 
 
-    
 
-  // this will pass on object (student list) once called
-  // export allows us to export this reuable funtions so we can use it other scripts
-  // start boostrap card
+
+
 
 // this is a reusable function that creates takes an array (studentList) and returns 
 const renderCards = (array) => {
   //creates an empty string for us to store the card info in
-  let refStuff = "";
+  let domString = "";
   // this iterates through an array (studentList) 
   array.forEach((item) => {
     //this adds the imported card boostrap function and add its to the empty array 
-    refStuff += card(item);
+    domString += studentCard(item);
   });
   // calls the renderToDom function and passes in our div of cards and the varable of reStuff which is holding out array data
-  renderToDom("#app", refStuff);
+  renderToDom("#app", domString);
 }
 
-// this is the end of the app use this to call all my functions into one function
-const startApp = () => {
-  // PUT ALL CARDS ON THE DOM
-  renderCards(studentList) // this puts the card on dom and passes in which array I need
 
-  // SELECT THE SEARCH INPUT
-  //document.querySelector('#searchInput').addEventListener('keyup', search) //takes in user input
 
-  // SELECT BUTTON ROW DIV
-  //document.querySelector('#btnRow').addEventListener('click', buttonFilter);
-}
-startApp();
+ // function to filter animals with specific favorite color this takes in an array and it takes in a string that we are checking
+  const filter = (array, houseName) => {
+  // creates anohter empty array to store our filterd items
+    const houseOfArray= [];
+  
+  // takes the array we added and iterates through
+    array.forEach((item) => {
+  // while iterating checking if item in array is equal to the type of animal that we are looking for with typeOf
+      if (item.house === houseName) {
+  // Pushes the newly assigned item to the array we selected
+        houseOfArray.push(item)
+      }
+    });
+  
+    // returns the new array only containng the filtered items 
+    return houseOfArray;
+  };
+
+  const showAllButton = document.querySelector("#show-btn");
+  const griffButton = document.querySelector("#griff-button");
+  const raveButton = document.querySelector("#rave-button")
+  const huffButton = document.querySelector("#huff-button")
+  const snakeButton = document.querySelector("#snake-button")
+
+  showAllButton.addEventListener("click", () => {
+    // this renders the type of pet we have selected at the top of the screen
+      renderCards(studentList)
+    });
+
+    griffButton.addEventListener("click", ()=> {
+      const griffHouse = filter(studentList, "Griffindor")
+      renderCards(griffHouse)
+    })
+
+    raveButton.addEventListener("click", ()=> {
+      const ravenHouse = filter(studentList, "Ravenclaw")
+      renderCards(ravenHouse)
+      console.log("Hello")
+    })
+
+    huffButton.addEventListener("click", ()=> {
+      const ravenHouse = filter(studentList, "Hufflepuff")
+      renderCards(ravenHouse)
+      console.log("Hello")
+    })
+
+    snakeButton.addEventListener("click", ()=> {
+      const ravenHouse = filter(studentList, "Snake")
+      renderCards(ravenHouse)
+      console.log("Hello")
+    })
+   // this puts the card on dom and passes in which array I need
