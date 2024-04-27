328492980e0b8bb3568d2291d6158da74bd62fb6
Trevor Mustoe <trevormustoe0405@gmail.com>
2024-04-27 12:27:22 -0400

    Created all my starter files and set up a fake db

---
+++ data/reference.js
@@ -0,0 +1,22 @@
+export const studentList = [
+{
+  id: 1,
+  student: "Harry",
+  house: "Griffindor"
+},
+{
+  id: 2,
+  student: "Ron",
+  house: "Ravenclaw",
+},
+{
+  id: 3,
+  student: "Jeff",
+  house: "Hufflepuff",
+},
+{
+  id: 4,
+  student:  "John Cena",
+  house: "Slytherine",
+}
+]
